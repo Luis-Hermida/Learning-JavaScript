b1e244813d116b4f4f8e9b516759ed100bd3598f
Luis Alejandro Hermida <42404237+Luis-Hermida@users.noreply.github.com>
2021-05-29 02:23:50 -0500

    Update 8_promises.js with an example of promises chaining

--- node/challenges/8_promises.js
+++ node/challenges/8_promises.js
@@ -14,7 +14,7 @@ doWorkCallback((error, result) => {
 // Promises Example
 const doWorkPromise = new Promise((resolve, reject) => {
   setTimeout(() => {
-    resolve("Sucess!");
+    resolve("Success!");
     reject("Error!");
   }, 2000);
 });
@@ -26,3 +26,42 @@ doWorkPromise
   .catch((error) => {
     console.log(error);
   });
+
+// Promise Chaining
+const add = (a, b) => {
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+      resolve(a + b);
+    }, 2000);
+  });
+};
+
+add(1, 2)
+  .then((sum) => {
+    console.log(sum);
+    return add(sum, 4);
+  })
+  .then((sum2) => {
+    console.log(sum2);
+  })
+  .catch((error) => {
+    console.log(error);
+  });
+
+// Bad approach
+// add(1, 2)
+//   .then((sum) => {
+//     console.log(sum);
+//     // Nested promise
+//     add(sum, 2)
+//       .then((sum2) => {
+//         console.log(sum2);
+//       })
+//       .catch((error) => {
+//         console.log(error);
+//       });
+//     ////
+//   })
+//   .catch((error) => {
+//     console.log(error);
+//   });
